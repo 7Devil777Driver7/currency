e5191e3f808e8591629713d520465a9894ebcbdb
Vitalii Novikov <vitaliinovikov07@gmail.com>
2021-11-19 17:34:11 +0200

    Commit after TravisCI initial check

--- app/currency/models.py
+++ app/currency/models.py
@@ -5,7 +5,7 @@ class Rate(models.Model):
     buy = models.DecimalField(max_digits=6, decimal_places=2)
     sale = models.DecimalField(max_digits=6, decimal_places=2)
     created = models.DateTimeField()
-    type = models.CharField(max_length=3)
+    cur_type = models.CharField(max_length=3)
     source = models.CharField(max_length=25, default='Unknown')
 
 
