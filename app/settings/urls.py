16195ca951bf2bfa94b230e69747b14bc5f77c60
Vitalii Novikov <vitaliinovikov07@gmail.com>
2021-11-28 11:29:52 +0200

    Model "Source" created. CRUD operations implemented.

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -13,8 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from currency.views import contact_us
-from currency.views import rate_list
+from currency.views import contact_us, rate_list, source_create, source_delete, source_list, source_update
+
 
 from django.contrib import admin
 from django.urls import path
@@ -22,6 +22,10 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('contact-us', contact_us),
-    path('rate-list', rate_list)
+    path('contact-us/', contact_us),
+    path('rate-list/', rate_list),
+    path('source-list/', source_list),
+    path('source-list/create/', source_create),
+    path('source-list/update/<int:pk>/', source_update),
+    path('source-list/delete/<int:pk>/', source_delete)
 ]
