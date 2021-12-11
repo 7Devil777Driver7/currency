7eb8f406cc6d8c971b84008437bdedff65f2b7a2
Vitalii Novikov <vitaliinovikov07@gmail.com>
2021-12-11 21:08:44 +0200

    Models added to admin.py. Permission restrictions implemented

--- app/currency/admin.py
+++ app/currency/admin.py
@@ -1,1 +1,65 @@
-# Register your models here.
+from currency.models import ContactUs, Rate, Source
+
+from django.contrib import admin
+
+
+class RateAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'buy',
+        'sale',
+        'created',
+        'cur_type',
+        'source'
+    )
+
+    list_filter = [
+        'cur_type'
+    ]
+
+    search_fields = (
+        'buy',
+        'sale',
+        'cur_type',
+        'source'
+    )
+
+    readonly_fields = (
+        'buy',
+        'sale'
+    )
+
+
+class SourceAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'source_url',
+        'name'
+        )
+
+    list_filter = [
+        'name'
+    ]
+
+
+class ContactUsAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'email_from',
+        'subject',
+        'message'
+        )
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+admin.site.register(Rate, RateAdmin)
+admin.site.register(Source, SourceAdmin)
+admin.site.register(ContactUs, ContactUsAdmin)
