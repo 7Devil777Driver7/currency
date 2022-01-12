1f21be17615e2859fba0d12aaa3131330dcd3dde
Vitalii Novikov <vitaliinovikov07@gmail.com>
2022-01-12 21:08:50 +0200

    Password reset flow implemented. Rate details can be viewed only by logged in users. Rate update/delete available for superusers only

--- app/currency/urls.py
+++ app/currency/urls.py
@@ -1,5 +1,5 @@
 from currency.views import (
-    ContactUsCreateView, ContactUsView, RateCreateView, RateDeleteView, RateListView,
+    ContactUsCreateView, ContactUsView, ProfileView, RateCreateView, RateDeleteView, RateDetailsView, RateListView,
     RateUpdateView, SourceCreateView, SourceDeleteView, SourceListView, SourceUpdateView)
 
 from django.urls import path
@@ -11,10 +11,12 @@ urlpatterns = [
     path('contact-us/create/', ContactUsCreateView.as_view(), name='contactus-create'),
     path('rate-list/', RateListView.as_view(), name='rate-list'),
     path('rate-list/create/', RateCreateView.as_view(), name='rate-create'),
+    path('rate-list/details/<int:pk>/', RateDetailsView.as_view(), name='rate-details'),
     path('rate-list/update/<int:pk>/', RateUpdateView.as_view(), name='rate-update'),
     path('rate-list/delete/<int:pk>/', RateDeleteView.as_view(), name='rate-delete'),
     path('source-list/', SourceListView.as_view(), name='source-list'),
     path('source-list/create/', SourceCreateView.as_view(), name='source-create'),
     path('source-list/update/<int:pk>/', SourceUpdateView.as_view(), name='source-update'),
-    path('source-list/delete/<int:pk>/', SourceDeleteView.as_view(), name='source-delete')
+    path('source-list/delete/<int:pk>/', SourceDeleteView.as_view(), name='source-delete'),
+    path('profile/<int:pk>/', ProfileView.as_view(), name='profile')
 ]
