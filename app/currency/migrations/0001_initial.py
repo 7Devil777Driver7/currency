8b73a5bab172e89a079a267f1643e61f8a2896e0
Vitalii Novikov <91780053+7Devil777Driver7@users.noreply.github.com>
2022-01-17 19:42:04 +0200

    Create 0001_initial.py

---
+++ app/currency/migrations/0001_initial.py
@@ -0,0 +1,53 @@
+import django.utils.timezone
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='ContactUs',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('created', models.DateTimeField(default=django.utils.timezone.now)),
+                ('name', models.CharField(default='User', max_length=255)),
+                ('email_from', models.EmailField(max_length=254)),
+                ('subject', models.CharField(max_length=255)),
+                ('message', models.TextField(max_length=1024)),
+                ('raw_content', models.TextField(default='')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Rate',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('buy', models.DecimalField(decimal_places=2, max_digits=6)),
+                ('sale', models.DecimalField(decimal_places=2, max_digits=6)),
+                ('created', models.DateTimeField(default=django.utils.timezone.now)),
+                ('cur_type', models.PositiveSmallIntegerField(choices=[(1, 'Dollar'), (2, 'Euro')], default=1)),
+                ('source', models.CharField(default='Unknown', max_length=25)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='RequestResponseLog',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('path', models.CharField(max_length=255)),
+                ('request_method', models.CharField(choices=[('GET', 'GET'), ('POST', 'POST')], default='GET', max_length=255)),  # noqa
+                ('response_time', models.PositiveSmallIntegerField()),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Source',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('source_url', models.CharField(max_length=255)),
+                ('name', models.CharField(max_length=64)),
+            ],
+        ),
+    ]
